c84b4609e424633fc4ec72103980819db5ffcf76
jay-lewis <justin94lewis@gmail.com>
2018-07-12 12:03:46 -0500

    adding jupyter notebooks for compressed sensing + phase retrieval

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -59,7 +59,8 @@ def get_image_grid(images_np, nrow=8):
     
     return torch_grid.numpy()
 
-def plot_image_grid(images_np, nrow =8, factor=1, interpolation='lanczos'):
+def plot_image_grid(images_np, nrow =8, factor=1, interpolation='lanczos',
+                    save=False, directory='~/', filename='random.png'):
     """Draws images in a grid
     
     Args:
@@ -72,7 +73,6 @@ def plot_image_grid(images_np, nrow =8, factor=1, interpolation='lanczos'):
     assert (n_channels == 3) or (n_channels == 1), "images should have 1 or 3 channels"
     
     images_np = [x if (x.shape[0] == n_channels) else np.concatenate([x, x, x], axis=0) for x in images_np]
-
     grid = get_image_grid(images_np, nrow)
     
     plt.figure(figsize=(len(images_np) + factor, 12 + factor))
@@ -81,24 +81,33 @@ def plot_image_grid(images_np, nrow =8, factor=1, interpolation='lanczos'):
         plt.imshow(grid[0], cmap='gray', interpolation=interpolation)
     else:
         plt.imshow(grid.transpose(1, 2, 0), interpolation=interpolation)
-    
+
+    if(save):
+        plt.savefig(directory+filename)
+
     plt.show()
     
     return grid
 
-def load(path):
+def load(path, img_type):
     """Load PIL image."""
-    img = Image.open(path)
+    if(img_type == 'rgb'):
+        img = Image.open(path)
+    elif(img_type == 'grayscale'):
+        img = Image.open(path).convert('L')
+    else:
+        raise ValueError("img_type: "+str(img_type) + " is not supported..")
     return img
 
-def get_image(path, imsize=-1):
+def get_image(path, imsize=-1, img_type='rgb'):
     """Load an image and resize to a cpecific size. 
 
     Args: 
         path: path to image
         imsize: tuple or scalar with dimensions; -1 for `no resize`
+        img_type: 'grayscale' or 'rgb' or ...
     """
-    img = load(path)
+    img = load(path, img_type)
 
     if isinstance(imsize, int):
         imsize = (imsize, imsize)
@@ -180,12 +189,15 @@ def np_to_pil(img_np):
 
     return Image.fromarray(ar)
 
-def np_to_torch(img_np):
+def np_to_torch(img_np,add_dim_flag=True):
     '''Converts image in numpy.array to torch.Tensor.
 
     From C x W x H [0..1] to  C x W x H [0..1]
     '''
-    return torch.from_numpy(img_np)[None, :]
+    if(add_dim_flag):
+        return torch.from_numpy(img_np)[None, :]
+    else:
+        return torch.from_numpy(img_np)
 
 def torch_to_np(img_var):
     '''Converts an image in torch.Tensor format to np.array.
@@ -205,6 +217,8 @@ def optimize(optimizer_type, parameters, closure, LR, num_iter):
         LR: learning rate
         num_iter: number of iterations 
     """
+    losses = []
+
     if optimizer_type == 'LBFGS':
         # Do several steps with adam first
         optimizer = torch.optim.Adam(parameters, lr=0.001)
@@ -226,7 +240,87 @@ def optimize(optimizer_type, parameters, closure, LR, num_iter):
         
         for j in range(num_iter):
             optimizer.zero_grad()
-            closure()
+            total_loss = closure()
+            losses.append(total_loss)
             optimizer.step()
     else:
         assert False
+
+    return losses
+
+
+def pad_zeros(a, b_shape):
+    b = np.zeros(b_shape)
+    b[:a.shape[0], :a.shape[1]] = a
+
+    return b
+
+
+def tensordot_pytorch(a, b, axes=2):
+    # code adapted from numpy
+    try:
+        iter(axes)
+    except Exception:
+        axes_a = list(range(-axes, 0))
+        axes_b = list(range(0, axes))
+    else:
+        axes_a, axes_b = axes
+    try:
+        na = len(axes_a)
+        axes_a = list(axes_a)
+    except TypeError:
+        axes_a = [axes_a]
+        na = 1
+    try:
+        nb = len(axes_b)
+        axes_b = list(axes_b)
+    except TypeError:
+        axes_b = [axes_b]
+        nb = 1
+
+    # uncomment in pytorch >= 0.5
+    # a, b = torch.as_tensor(a), torch.as_tensor(b)
+    as_ = a.shape
+    nda = a.dim()
+    bs = b.shape
+    ndb = b.dim()
+    equal = True
+    if na != nb:
+        equal = False
+    else:
+        for k in range(na):
+            if as_[axes_a[k]] != bs[axes_b[k]]:
+                equal = False
+                break
+            if axes_a[k] < 0:
+                axes_a[k] += nda
+            if axes_b[k] < 0:
+                axes_b[k] += ndb
+    if not equal:
+        raise ValueError("shape-mismatch for sum")
+
+    # Move the axes to sum over to the end of "a"
+    # and to the front of "b"
+    notin = [k for k in range(nda) if k not in axes_a]
+    newaxes_a = notin + axes_a
+    N2 = 1
+    for axis in axes_a:
+        N2 *= as_[axis]
+    newshape_a = (int(np.multiply.reduce([as_[ax] for ax in notin])), N2)
+    olda = [as_[axis] for axis in notin]
+
+    notin = [k for k in range(ndb) if k not in axes_b]
+    newaxes_b = axes_b + notin
+    N2 = 1
+    for axis in axes_b:
+        N2 *= bs[axis]
+    newshape_b = (N2, int(np.multiply.reduce([bs[ax] for ax in notin])))
+    oldb = [bs[axis] for axis in notin]
+
+    at = a.permute(newaxes_a).reshape(newshape_a)
+    bt = b.permute(newaxes_b).reshape(newshape_b)
+
+    res = at.matmul(bt)
+
+
+    return res.reshape(olda + oldb)
